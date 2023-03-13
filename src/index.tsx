c9fe3199a9cc459ba525d957cce4c466bea659df
violent-code <aidasbuivydas@gmail.com>
2023-03-13 21:37:18 +0200

    feat: implement React router

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,14 @@ import './sass/global.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import { RouteManager } from './routes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <div className="w-screen h-screen bg-red-300 text-3xl ">
-      Hi Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi nemo, sed
-      reprehenderit fugit earum assumenda culpa repudiandae itaque dolores dolore! Velit
-      numquam repudiandae tempora odit autem fuga cupiditate placeat quasi.
-    </div>
+    <Router>
+      <RouteManager />
+    </Router>
   </React.StrictMode>,
 );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="w-screen h-screen bg-red-300 text-3xl ">
+      Hi Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi nemo, sed
+      reprehenderit fugit earum assumenda culpa repudiandae itaque dolores dolore! Velit
+      numquam repudiandae tempora odit autem fuga cupiditate placeat quasi.
+    </div>
+  );
 };
 
 export default Home;
