e488bdb4445689aa87ead2bd18d2d7ed563e021a
violent-code <aidasbuivydas@gmail.com>
2023-03-13 21:00:19 +0200

    feat: rename project, add component indexes, configure React router

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,12 @@ import './sass/global.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <div className="w-screen h-screen bg-red-300 text-3xl ">
+      Hi Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi nemo, sed
+      reprehenderit fugit earum assumenda culpa repudiandae itaque dolores dolore! Velit
+      numquam repudiandae tempora odit autem fuga cupiditate placeat quasi.
+    </div>
   </React.StrictMode>,
 );
