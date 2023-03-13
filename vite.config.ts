eec626c6a0b5b97fb6f31fb2b40821a1f0706107
violent-code <aidasbuivydas@gmail.com>
2023-03-13 04:46:37 +0200

    feat: add tab icon, svgo implementation, add vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 // import eslintPlugin from '@nabla/vite-plugin-eslint';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import svg from 'vite-plugin-svgo';
 import webfontDownload from 'vite-plugin-webfont-dl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), webfontDownload()],
+  build: {
+    rollupOptions: {
+      input: {
+        app: './public/index.html',
+      },
+    },
+    outDir: '../dist',
+  },
+  plugins: [react(), webfontDownload(), svg()],
 });
