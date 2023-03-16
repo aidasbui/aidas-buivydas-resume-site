8837305dbf919a63eeeb22ed012b5779c5f3b6c5
aidasbui <aidasbuivydas@gmail.com>
2023-03-16 19:08:37 +0200

    feat: add text content to Home page, animate span

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,33 @@
+import { motion } from 'framer-motion';
 import React from 'react';
 
+import { TTextContent } from '../../types/text-content';
+
 const Home = () => {
+  const heroHeading: TTextContent = `Welcome`;
+
+  const heroParagraph: TTextContent = `You've encountered my personal website.
+
+  I'm a front-end web developer, who works on bringing eye-pleasing UI solutions to end users with the help of React, TypeScript and a load of other tools.
+  
+  I also make music! Feel free to check out a few of my recent DJ sets on MixCloud.`;
+
   return (
-    <div className="h-screen w-full bg-color-primary">
-      <h1>Hi!</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi nemo, sed
-        reprehenderit fugit earum assumenda culpa repudiandae itaque dolores dolore! Velit
-        numquam repudiandae tempora odit autem fuga cupiditate placeat quasi.
-      </p>
+    <div className=" w-full">
+      <section>
+        <h2 className="py-4">
+          {heroHeading}
+          <motion.span
+            className="inline-block"
+            initial={{ y: 0 }}
+            animate={{ y: '-0.1rem' }}
+            transition={{ duration: 0.8, repeat: Infinity, repeatType: 'reverse' }}
+          >
+            !
+          </motion.span>
+        </h2>
+        <p className="whitespace-pre-line">{heroParagraph}</p>
+      </section>
     </div>
   );
 };
