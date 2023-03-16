9c931a74fe76970dc2559ed839f552cac6eee698
aidasbui <aidasbuivydas@gmail.com>
2023-03-16 15:39:09 +0200

    feat: declare color, font vars, implement base css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,30 @@
-    /** @type {import('tailwindcss').Config} */
-    module.exports = {
-      content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-      ],
-      darkMode: 'class',
-      theme: {
-      fontFamily: {
-      'sans': ['JetBrains Mono', 'sans-serif'],
-      'mono': ['VT323', 'sans-serif'],
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    colors: {
+      'color-primary': '#230D27',
+      'color-secondary': '#DDDD21',
+      'color-purple-800': '#433447',
+      'color-purple-700': '#826C86',
+      'color-purple-600': '#5D4962',
+      'color-purple-500': '#B49BBB',
+      'color-white': '#FAFAFA',
     },
-     extend: {},
-     },
-    plugins: [],
-   }
+    fontFamily: {
+      sans: '"JetBrains Mono"',
+    },
+    fontSize: {
+      sm: ['1rem', { lineHeight: '1.75' }],
+      base: ['1.25rem', { lineHeight: '1.7' }],
+      lg: ['1.56rem', { lineHeight: '1.64' }],
+      xl: ['2rem', { lineHeight: '1.625' }],
+      '2xl': ['2.56rem', { lineHeight: '1.59' }],
+      '3xl': ['3.25rem', { lineHeight: '1.56' }],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
