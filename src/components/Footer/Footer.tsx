be1e1806558fa7d6b8a882cf05acc9b57c3cb548
aidasbui <aidasbuivydas@gmail.com>
2023-03-19 18:25:10 +0200

    feat: mockup footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="h-8 border-b-2 border-color-purple-700 bg-color-primary"></footer>
+    <footer className="flex h-8 items-center justify-center p-5 font-vt323 ">
+      <p>Licensed under MIT</p>
+    </footer>
   );
 };
 
