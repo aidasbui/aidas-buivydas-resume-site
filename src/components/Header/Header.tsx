2df37e8ca03b968e14c58db1c5d2a8f3e84cd648
aidasbui <aidasbuivydas@gmail.com>
2023-03-19 18:23:29 +0200

    feat: add semantic HTML for header photo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="sticky top-0 left-0 z-50 flex h-full flex-col items-center justify-center border-b-2 border-color-purple-700 bg-background-noise bg-cover bg-center py-4 px-12 text-center shadow-xl">
-      <div className="h-28 w-28 rounded-full border-2 border-color-purple-700 bg-color-purple-500 bg-aidas-bui-photo bg-cover bg-center "></div>
+    <header className="sticky top-0 left-0 z-50 flex h-full flex-col items-center justify-center border-b-2 border-color-purple-700 py-4 px-12 text-center shadow-xl">
+      <div className="h-28 w-28 rounded-full border-2 border-color-purple-700 bg-color-purple-500">
+        <img
+          className="h-full w-full rounded-full object-cover"
+          src="/src/assets/images/aidas-buivydas.jpeg"
+          alt="Aidas Buivydas"
+        />
+      </div>
       <h1 className="py-2">Aidas Buivydas</h1>
       <div className=" font-vt323 text-xl">Front-end Web Developer</div>
     </header>
