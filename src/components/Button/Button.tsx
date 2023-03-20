a393a681f52276237d13f2ca20d11eb4fc485189
aidasbui <aidasbuivydas@gmail.com>
2023-03-20 19:33:52 +0200

    feat: add reusable Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,70 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-type Props = {
-  text: string;
+type TButtonProps = {
+  children: React.ReactNode;
+  type?: 'submit' | 'reset' | 'button' | undefined;
+  to?: string;
+  href?: string;
+  ariaLabel?: string;
+  onClick?: React.MouseEventHandler;
+  disabled?: boolean;
 };
 
-const Button = ({ text }: Props) => {
-  return (
-    <button className="w-36 rounded-xl border-2 border-color-purple-700 bg-color-secondary p-2 font-vt323 text-2xl leading-none text-color-primary transition-colors duration-75 hover:border-color-purple-800 hover:bg-color-secondary/70 hover:text-color-purple-800">
-      {text}
-    </button>
-  );
+const Button = ({
+  children,
+  type = 'button',
+  to,
+  href,
+  ariaLabel,
+  onClick,
+  disabled = false,
+}: TButtonProps) => {
+  const outerLink = href?.includes('.');
+  const innerLink = href?.includes('#');
+
+  const buttonStyles =
+    'w-36 rounded-xl border-2 border-color-purple-700 bg-color-secondary p-2 font-vt323 text-2xl leading-none text-color-primary transition-colors duration-75 hover:border-color-purple-800 hover:bg-color-secondary/70 hover:text-color-purple-800 block';
+
+  {
+    if (outerLink) {
+      return (
+        <a
+          href={href}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label={ariaLabel}
+          className={buttonStyles}
+        >
+          {children}
+        </a>
+      );
+    } else if (innerLink) {
+      return (
+        <a href={href} target="_self" aria-label={ariaLabel} className={buttonStyles}>
+          {children}
+        </a>
+      );
+    } else if (to) {
+      return (
+        <Link to={to} aria-label={ariaLabel} className={buttonStyles}>
+          {children}
+        </Link>
+      );
+    } else {
+      return (
+        <button
+          type={type}
+          onClick={onClick}
+          aria-label={ariaLabel}
+          disabled={disabled}
+          className={buttonStyles}
+        >
+          {children}
+        </button>
+      );
+    }
+  }
 };
 
 export default Button;
