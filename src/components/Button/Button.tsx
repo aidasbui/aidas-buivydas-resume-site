ae9bc2f86a894cad45746cdf3ce4125f1a2c737e
aidasbui <aidasbuivydas@gmail.com>
2023-03-24 01:40:06 +0200

    feat: implement file download link in Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,6 +9,7 @@ type TButtonProps = {
   ariaLabel?: string;
   onClick?: React.MouseEventHandler;
   disabled?: boolean;
+  download?: string;
 };
 
 const Button = ({
@@ -19,15 +20,30 @@ const Button = ({
   ariaLabel,
   onClick,
   disabled = false,
+  download,
 }: TButtonProps) => {
   const outerLink = href?.includes('.');
   const innerLink = href?.includes('#');
+  const localFile = href?.includes('assets/files/');
 
   const buttonStyles =
     'inline-block flex justify-center items-center w-36 rounded-xl border-2 border-color-purple-700 bg-color-secondary p-2 font-vt323 text-2xl leading-none text-color-primary transition-colors duration-75 hover:border-color-purple-500 block hover:brightness-[105%] focus:outline-dashed focus:outline-offset-4 focus:outline-color-secondary';
 
   {
-    if (outerLink) {
+    if (localFile) {
+      return (
+        <a
+          href={href}
+          download={download}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label={ariaLabel}
+          className={`${buttonStyles} w-44`}
+        >
+          {children}
+        </a>
+      );
+    } else if (outerLink) {
       return (
         <a
           href={href}
