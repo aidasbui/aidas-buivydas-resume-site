65d9c11ef361fabaa3f03b932b5a1fc71080d3b7
aidasbui <aidasbuivydas@gmail.com>
2023-03-23 14:17:04 +0200

    feat: implement ScrollToTop on route change

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,32 @@
 import './styles/global.css';
 
-import React from 'react';
+import React, { PropsWithChildren, useLayoutEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, useLocation } from 'react-router-dom';
 
 import Layout from './components/Layout';
 import { RouteManager } from './routes';
 
+const ScrollToTop = ({ children }: PropsWithChildren) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'instant', // Optional if you want to skip the scrolling animation. About the any: https://github.com/microsoft/TypeScript-DOM-lib-generator/issues/1195
+    } as any); // eslint-disable-line
+  }, [location.pathname]);
+  return <>{children}</>;
+};
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
-      <Layout>
-        <RouteManager />
-      </Layout>
+      <ScrollToTop>
+        <Layout>
+          <RouteManager />
+        </Layout>
+      </ScrollToTop>
     </Router>
   </React.StrictMode>,
 );
