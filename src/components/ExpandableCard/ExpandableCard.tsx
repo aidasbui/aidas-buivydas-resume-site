2a19792b793937d188f2c6da2357de2774fad075
aidasbui <aidasbuivydas@gmail.com>
2023-03-23 14:15:12 +0200

    feat: add ExpandableCard

--- src/components/ExpandableCard/ExpandableCard.tsx
+++ src/components/ExpandableCard/ExpandableCard.tsx
@@ -6,12 +6,12 @@ import { ReactComponent as ChevronUp_SVG } from '../../assets/icons/chevron-up.s
 import { useLevitate as Levitate } from '../../hooks/useLevitate';
 
 type Props = {
-  children?: ReactNode[] | ReactNode;
+  children?: ReactNode;
   title: string;
   renderLeftIcon: () => JSX.Element;
 };
 
-const ExpandableCardButton = ({ title, renderLeftIcon }: Props) => {
+const ExpandableCard = ({ title, renderLeftIcon }: Props) => {
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
   const [isHovered, setIsHovered] = useState<boolean>(false);
 
@@ -31,48 +31,53 @@ const ExpandableCardButton = ({ title, renderLeftIcon }: Props) => {
     <motion.div
       className={`${
         isExpanded && expandedCardStyles
-      } h-full w-full max-w-sm transform-gpu rounded-xl border-2 bg-color-purple-800 px-4 transition-colors duration-75 hover:bg-color-purple-600 active:bg-color-purple-500
+      } h-full w-full max-w-sm transform-gpu overflow-hidden rounded-xl border-2 bg-color-purple-800 px-4 shadow-2xl transition-colors duration-75 hover:bg-color-purple-600 active:border-color-secondary active:bg-color-purple-600
   `}
       whileHover={{ scale: isExpanded ? 1 : 1.03 }}
       whileTap={{
-        scale: 0.97,
+        scale: isExpanded ? 1 : 0.97,
       }}
+      style={{ borderRadius: '0.75rem' }}
     >
       <motion.button
+        key="expandable-card-button"
         aria-label={isExpanded ? `Expand ${title} card` : `Collapse ${title} card`}
         aria-controls="expandable card"
         aria-expanded={isExpanded ? 'true' : 'false'}
-        className={`h-16 w-full items-center justify-between rounded-xl border-2 border-none text-center    ${
+        className={`h-16 w-full items-center justify-between border-none text-center ${
           isExpanded && expandedButtonStyles
         }`}
         onClick={expandCardHandler}
         onMouseOver={levitateIconHandler}
         onMouseOut={levitateIconHandler}
       >
-        <div className="leading-0 flex items-center justify-between">
-          <div className="flex justify-start gap-4">
+        <motion.div className="leading-0 flex items-center justify-between">
+          <motion.div className="flex justify-start gap-4">
             <>{isHovered ? <Levitate>{renderLeftIcon()}</Levitate> : renderLeftIcon()}</>
             <h3 className="p-0 text-xl">{title}</h3>
-          </div>
+          </motion.div>
           {isExpanded ? (
             <ChevronUp_SVG className="w-6" />
           ) : (
             <ChevronDown_SVG className="w-6" />
           )}
-        </div>
+        </motion.div>
       </motion.button>
+
       {isExpanded && (
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id delectus eius dolor
-          unde. Tempora natus, consequatur consequuntur debitis doloribus soluta. Fugit
-          inventore eveniet molestiae ipsum earum quas alias ab accusantium. Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Ducimus, quod! Suscipit sequi est
-          dolorem obcaecati quibusdam in cupiditate ullam hic explicabo voluptates, autem
-          velit facilis architecto eaque distinctio, ipsum non.
-        </p>
+        <motion.div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id delectus eius
+            dolor unde. Tempora natus, consequatur consequuntur debitis doloribus soluta.
+            Fugit inventore eveniet molestiae ipsum earum quas alias ab accusantium. Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Ducimus, quod! Suscipit
+            sequi est dolorem obcaecati quibusdam in cupiditate ullam hic explicabo
+            voluptates, autem velit facilis architecto eaque distinctio, ipsum non.
+          </p>
+        </motion.div>
       )}
     </motion.div>
   );
 };
 
-export default ExpandableCardButton;
+export default ExpandableCard;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export * from './Button';
-export * from './ExpandableCardButton';
+export * from './ExpandableCard';
 export * from './Footer';
 export * from './Header';
 export * from './Layout';
