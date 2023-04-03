69a2965a4c24bcb0360243eaf3033dc9a66298d3
aidasbui <aidasbuivydas@gmail.com>
2023-04-03 13:17:53 +0300

    feat: add courses and skills sections' data

---
+++ src/pages/Home/courses.ts
@@ -0,0 +1,27 @@
+type Tcourses = {
+  title: string;
+  inProgress: boolean;
+}[];
+
+export const courses: Tcourses = [
+  {
+    title: 'Understanding Typescript, Academind GmbH',
+    inProgress: false,
+  },
+  {
+    title: 'React and TypeScript (v2), Frontend Masters',
+    inProgress: false,
+  },
+  {
+    title: 'React - The Complete Guide, Academind GmbH',
+    inProgress: true,
+  },
+  {
+    title: 'JavaScript - The Complete Guide, Academind GmbH',
+    inProgress: false,
+  },
+  {
+    title: 'CSS - The Complete Guide, Academind GmbH',
+    inProgress: false,
+  },
+];
---
+++ src/pages/Home/skills.ts
@@ -0,0 +1,19 @@
+type Tskills = string[];
+
+export const skills: Tskills = [
+  'React.js',
+  'TypeScript',
+  'JavaScript',
+  'Semantic HTML',
+  'CSS',
+  'SASS',
+  'Tailwind CSS',
+  'Git',
+  'Agile Methodologies',
+  'BEM',
+  'Arch Linux',
+  'WebPack',
+  'Vite.js',
+  'Web Project Structure',
+  'Dynamic Components',
+];
