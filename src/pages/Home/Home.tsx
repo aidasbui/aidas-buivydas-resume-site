446477ec8ba70d55a718f3544fe938116abe6000
aidasbui <aidasbuivydas@gmail.com>
2023-04-06 21:20:14 +0300

    feat: render Navigation and Card; make responsive

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import Card from '../../components/Card/Card';
 import ContactsSection from '../../components/ContactsSection';
+import Navigation from '../../components/Navigation/Navigation';
+import useMediaQuery from '../../hooks/useMediaQuery';
 import { ParallaxScroll } from '../../hooks/useParallaxScroll';
+import { renderCompletedCourses } from './ExpandableCards/coursesSectionData';
 import ExpandableCards from './ExpandableCards/ExpandableCards';
 import HomeHeroSection from './HomeHeroSection/HomeHeroSection';
 
 const Home = () => {
+  const isDesktop = useMediaQuery('(min-width: 1024px)');
+
   return (
     <div className="relative flex w-full flex-col gap-12">
       {/* TODO: implement prefers reduced motion with framer-motion */}
@@ -13,16 +19,33 @@ const Home = () => {
         <HomeHeroSection />
       </ParallaxScroll>
 
-      <ParallaxScroll transformPercentOutput={['0%', '-5%']}>
-        <ExpandableCards />
-      </ParallaxScroll>
+      <div className="flex w-full flex-col">
+        <ParallaxScroll transformPercentOutput={['0%', '-5%']}>
+          {isDesktop ? (
+            <div>
+              <Navigation />
+              <Card>
+                {
+                  <>
+                    <h2 className="pb-10">Section Title</h2>
+                    {renderCompletedCourses()}
+                  </>
+                }
+              </Card>
+            </div>
+          ) : (
+            <ExpandableCards />
+          )}
+        </ParallaxScroll>
 
-      {/* TODO: conditionally render the below Parallax Scroll + ContactsSection code with useMediaQuery depending on screen size */}
-      <ParallaxScroll transformPercentOutput={['0', '-50%']}>
-        <div className="px-[1.1rem]">
-          <ContactsSection />
-        </div>
-      </ParallaxScroll>
+        {!isDesktop && (
+          <div className="flex justify-start px-[1.1rem]">
+            <ParallaxScroll transformPercentOutput={['0', '-50%']}>
+              <ContactsSection />
+            </ParallaxScroll>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
