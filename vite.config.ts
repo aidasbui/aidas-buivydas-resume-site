af6f8bf1b124f25f49ce287aab895208a58aa0dd
aidasbui <aidasbuivydas@gmail.com>
2023-04-19 17:13:34 +0300

    feat: configure Vite dev server port

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ import webfontDownload from 'vite-plugin-webfont-dl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   build: {
     rollupOptions: {
       input: {
