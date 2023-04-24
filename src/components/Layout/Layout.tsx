590e7a110fbc37a76012a3a97f6460801c8dabf0
aidasbui <aidasbuivydas@gmail.com>
2023-04-24 12:57:31 +0300

    fix: disable parallaxing background

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,40 +1,34 @@
-import {
-  AnimatePresence,
-  motion,
-  useScroll,
-  useSpring,
-  useTransform,
-} from 'framer-motion';
-import React, { PropsWithChildren, useLayoutEffect, useRef, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import React, { PropsWithChildren } from 'react';
 
 import Footer from '../Footer';
 import Header from '../Header';
 import ScrollProgress from './ScrollProgress';
 
 const Layout = ({ children }: PropsWithChildren) => {
-  const ref = useRef<HTMLDivElement | null>(null);
-  const [offsetTop, setOffsetTop] = useState(0);
-  const { scrollY } = useScroll();
+  // const ref = useRef<HTMLDivElement | null>(null);
+  // const [offsetTop, setOffsetTop] = useState(0);
+  // const { scrollY } = useScroll();
 
-  useLayoutEffect(() => {
-    if (!ref.current) return;
-    setOffsetTop(ref.current.offsetTop);
-  }, [ref]);
+  // useLayoutEffect(() => {
+  //   if (!ref.current) return;
+  //   setOffsetTop(ref.current.offsetTop);
+  // }, [ref]);
 
-  const y = useSpring(scrollY, {
-    stiffness: 300,
-    damping: 70,
-    mass: 0.1,
-  });
+  // const y = useSpring(scrollY, {
+  //   stiffness: 300,
+  //   damping: 70,
+  //   mass: 0.1,
+  // });
 
-  const yRange = useTransform(y, [offsetTop - 2000, offsetTop + 2000], ['0%', '5%']);
+  // const yRange = useTransform(y, [offsetTop - 2000, offsetTop + 2000], ['0%', '5%']);
 
   return (
     <div className="relative flex min-h-screen flex-col justify-between overflow-y-hidden">
       <motion.div
-        ref={ref}
+        // ref={ref}
         className="absolute -top-[50rem] -z-40 h-[600rem] w-full bg-color-primary bg-background-noise bg-center"
-        style={{ translateY: yRange }}
+        // style={{ translateY: yRange }}
       />
       <div className="left-0 top-0 z-50">
         <Header />
