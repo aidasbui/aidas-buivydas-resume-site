0e74659941461592e2e3f19cec59d20e004efcf3
aidasbui <aidasbuivydas@gmail.com>
2023-04-25 17:31:06 +0300

    fix: implement framer layout in TabbedCards

--- src/pages/Home/TabbedCards/TabbedCards.tsx
+++ src/pages/Home/TabbedCards/TabbedCards.tsx
@@ -1,7 +1,7 @@
 import Card from 'components/Card/Card';
 import Navigation from 'components/Navigation/Navigation';
+import { AnimatePresence, motion } from 'framer-motion';
 import React, { useState } from 'react';
-import ResizablePanel from 'utils/ResizablePanel';
 
 import { homeCardsData, TSectionTitle } from '../homeData/homeCardsData';
 
@@ -9,19 +9,35 @@ const TabbedCards = () => {
   const [activeTab, setActiveTab] = useState<TSectionTitle>('Courses');
 
   return (
-    <div>
+    <>
       <Navigation activeTab={activeTab} setActiveTab={setActiveTab} />
-      <Card>
-        <div className="flex w-full flex-col items-center justify-center">
-          <ResizablePanel title={activeTab} active={!!activeTab}>
-            <h1 className="self-center pb-8">{activeTab}</h1>
-            {homeCardsData
-              .find((sectionData) => sectionData.title === activeTab)
-              ?.content()}
-          </ResizablePanel>
-        </div>
-      </Card>
-    </div>
+      <motion.div layout="size">
+        <Card>
+          <motion.div
+            layout="position"
+            className="relative flex w-full flex-col items-center justify-center"
+          >
+            <AnimatePresence initial={false} mode="popLayout">
+              <motion.div
+                key={`${activeTab}-${Math.random()}`}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.2 }}
+                className="lg:flex lg:w-full lg:flex-col lg:items-start lg:justify-start"
+              >
+                <div className={`flex flex-col px-4 md:px-12 lg:w-full`}>
+                  <h1 className="self-center pb-8">{activeTab}</h1>
+                  {homeCardsData
+                    .find((sectionData) => sectionData.title === activeTab)
+                    ?.content()}
+                </div>
+              </motion.div>
+            </AnimatePresence>
+          </motion.div>
+        </Card>
+      </motion.div>
+    </>
   );
 };
 
