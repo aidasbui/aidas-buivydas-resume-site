daae32ae1bb8301dc3063d2add1d7c54eec16403
aidasbui <aidasbuivydas@gmail.com>
2024-01-04 13:34:34 +0200

    feat: update license year

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="flex h-8 items-center justify-center p-5 font-vt323 text-xl ">
-      <p>Copyright © 2023 Aidas Buivydas</p>
+      <p>Copyright © 2024 Aidas Buivydas</p>
     </footer>
   );
 };
