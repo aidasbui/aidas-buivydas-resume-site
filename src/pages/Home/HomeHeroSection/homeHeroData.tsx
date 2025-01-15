90ba6bf85af19ab42099d7c390760656e80870a7
aidasbui <aidasbuivydas@gmail.com>
2025-01-15 21:20:09 +0200

    fix: revert "feat: test automatic docker deployments"
    This reverts commit 2a4dce0743288c75988851a5e215c39c38b296b9.

--- src/pages/Home/HomeHeroSection/homeHeroData.tsx
+++ src/pages/Home/HomeHeroSection/homeHeroData.tsx
@@ -12,7 +12,6 @@ export const homeHeroData: THomeHeroData = {
   
           I'm a full-stack web developer, focusing on front-end, who works on bringing eye-pleasing UI solutions to end users with the help of Next.js, React, TypeScript and a load of other tools.
           
-          I also make music! You'll find a link to my DJ sets in the hobbies' section below.
-          (TEST TEST - Looks like GithubActions are working via a self-hosted runner!)`,
+          I also make music! You'll find a link to my DJ sets in the hobbies' section below.`,
   asset: AidasBuivydas_CV,
 };
